6a360cfeb30e33ba77ffa671d0c00e798e98cfa2
TOSHIBA1 <TOSHIBA1@TOSHIBA>
2016-12-16 15:56:45 +0200

    Add project files.

---
+++ ayoub wahib/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ayoub_wahib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ايوب وهيب");
+            Console.ReadKey();
+            
+      
+            }
+    }
+}
